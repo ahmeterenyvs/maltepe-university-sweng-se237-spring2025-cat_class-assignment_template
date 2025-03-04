04a9665d16af40fb8afe3171af9fb6d7a78b81b2
ahmeterenyvs <ahmeteren.metro09@gmail.com>
2025-03-04 15:51:35 +0300

    Update Simple_main.java

--- module1/src/tr/edu/maltepe/oop/Simple_main.java
+++ module1/src/tr/edu/maltepe/oop/Simple_main.java
@@ -1,10 +1,11 @@
 package tr.edu.maltepe.oop;
-public  class Simple_main {
 
- public static void main(String []args){
-        //
-       System.out.println("simple main.....");
-        }
+public static void main(String[] args) {
+    // Creating objects
+    Class obj1 = new Class("Alice", 25);
+    Class obj2 = new Class("Bob", 30);
+
+    // Calling the meow method
+    obj1.meow();
+    obj2.meow();
 }
-
-
